3b842a2d47d2c10eb51f9109be82299b957db007
mercuricchloride <mercuryprotocol@protonmail.com>
2024-01-15 11:05:25 -0800

    Type casts should be working

--- src/type_casts.rs
+++ src/type_casts.rs
@@ -1,8 +1,6 @@
-use std::str::FromStr;
-
-use alloy_primitives::*;
-
 use crate::prelude::*;
+use alloy_primitives::*;
+use std::str::FromStr;
 
 const ZERO_ADDRESS: Address = address!();
 
@@ -67,9 +65,9 @@ pub fn string<T: Into<SolidityType>>(value: T) -> SolidityType {
         SolidityType::Boolean(val) => {
             let val = val.to::<u8>();
             if val == 0 {
-                "true".to_string()
-            } else {
                 "false".to_string()
+            } else {
+                "true".to_string()
             }
         }
         SolidityType::Uint(val) => val.to_string(),
@@ -95,26 +93,34 @@ pub fn string<T: Into<SolidityType>>(value: T) -> SolidityType {
 
 pub fn uint<T: Into<SolidityType>>(value: T) -> SolidityType {
     let value: SolidityType = Into::into(value);
-    let string: String = match &value {
+    let value = match value {
         SolidityType::Boolean(val) => {
             let val = val.to::<u8>();
             if val == 0 {
-                "true".to_string()
+                Uint::from(0)
             } else {
-                "false".to_string()
+                Uint::from(1)
             }
         }
-        SolidityType::Uint(val) => val.to_string(),
-        SolidityType::Enum(val) => val.to_string(),
-        SolidityType::Address(val) => val.to_string(),
+        SolidityType::Uint(val) => val,
+        SolidityType::Enum(val) => Uint::from(val),
+        SolidityType::Address(val) => {
+            let value = val.into_array();
+            Uint::from_be_slice(&value[..])
+        }
         SolidityType::ByteArray(val) => {
             // NOTE I am not fully sure if this is correct.
             // The strings should be 0x prefixed. But not sure if that's always the case
-            val.to_string()
+            let word = val.0;
+            Uint::from_be_slice(&word[..])
         }
-        SolidityType::FixedArray(_) => todo!(),
-        SolidityType::String(_) => {
-            return value;
+        SolidityType::FixedArray(val) => Uint::from_be_slice(&val.0),
+        SolidityType::String(val) => {
+            if let Ok(val) = val.parse() {
+                val
+            } else {
+                return SolidityType::Null;
+            }
         }
         SolidityType::Tuple(_)
         | SolidityType::List(_)
@@ -122,5 +128,51 @@ pub fn uint<T: Into<SolidityType>>(value: T) -> SolidityType {
         | SolidityType::Null => return SolidityType::Null,
     };
 
-    SolidityType::String(string)
+    SolidityType::Uint(value)
+}
+
+pub fn bytes<T: Into<SolidityType>>(value: T) -> SolidityType {
+    let value: SolidityType = Into::into(value);
+    let value = match value {
+        SolidityType::Boolean(val) => {
+            let val = val.to::<u8>();
+            if val == 0 {
+                Bytes::copy_from_slice(&[0])
+            } else {
+                Bytes::copy_from_slice(&[1])
+            }
+        }
+        SolidityType::Uint(val) => {
+            let bytes: [u8; 32] = val.to_be_bytes();
+            Bytes::copy_from_slice(&bytes[..])
+        }
+        SolidityType::Enum(val) => {
+            let byte: u8 = val.to::<u8>();
+            Bytes::copy_from_slice(&[byte])
+        }
+        SolidityType::Address(val) => {
+            let value = val.into_array();
+            Bytes::copy_from_slice(&value[..])
+        }
+        SolidityType::ByteArray(val) => {
+            // NOTE I am not fully sure if this is correct.
+            // The strings should be 0x prefixed. But not sure if that's always the case
+            let word = val.0;
+            Bytes::copy_from_slice(&word[..])
+        }
+        SolidityType::FixedArray(val) => Bytes::copy_from_slice(&val.0),
+        SolidityType::String(val) => {
+            if let Ok(val) = val.parse() {
+                val
+            } else {
+                return SolidityType::Null;
+            }
+        }
+        SolidityType::Tuple(_)
+        | SolidityType::List(_)
+        | SolidityType::Struct(_)
+        | SolidityType::Null => return SolidityType::Null,
+    };
+
+    SolidityType::ByteArray(value)
 }
